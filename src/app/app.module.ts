c0cd23b9bc34c2752489ffc9d5387ce92f29d9b3
sahil <sahilpotdar747@gmail.com>
2025-07-20 19:39:08 +0530

    done with 2 page ui

---
+++ src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FeatherModule } from 'angular-feather';
+import { allIcons } from 'angular-feather/icons';
+import { FormsModule } from '@angular/forms'
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FullComponent } from './layouts/full/full.component';
+import { DemoFlexyModule } from './demo-flexy-module'
+
+
+// Modules
+import { DashboardModule } from './dashboard/dashboard.module';
+import { ComponentsModule } from './components/components.module';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AddPropertyComponent } from './add-property/add-property.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FullComponent,
+    LandingPageComponent,
+    PageNotFoundComponent,
+    AddPropertyComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FeatherModule.pick(allIcons),
+    DemoFlexyModule,
+    DashboardModule,
+    ComponentsModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
